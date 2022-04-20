fc26d954e93e3ea57c74577570203c5d2ec717d5
Dharmit Saradva <46236383+dharmit036@users.noreply.github.com>
2022-04-20 13:54:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -e "Current working directory:$1" 
 pm2 delete personal-portfolio || true
-PORT=1338 pm2 start $1/app.js -n personal-portfolio --kill-timeout 600000
+PORT=1338 pm2 start $1/app.js -n personal-portfolio --watch
 pm2 save
 pm2 restart all
 echo "Project deployed!"
